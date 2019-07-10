562d03084aa17547c4effac8bfc38ac4900b9444
Amaury AC <amaury.costa@hotmail.com>
2019-07-10 10:20:45 -0300

    Trabalhando com EJS e módulos externos no servidor NodeJS

--- Introducao_ao_Node_JS/app.js
+++ Introducao_ao_Node_JS/app.js
@@ -1,34 +1,18 @@
 var express = require('express');
+var meuModulo = require('./modulo_externo');
+
+console.log(meuModulo());
+
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-	res.send(`
-		<!DOCTYPE html>
-		<html>
-			<head>
-				<meta charset="utf-8">
-				<title>Introdução ao Node JS</title>
-			</head>
-			<body>
-				<h1>Página Home</h1>
-			</body>
-		</html>
-	`);
+	res.render('site/home');
 });
 
 app.get('/contato', function(req, res) {
-	res.send(`
-		<!DOCTYPE html>
-		<html>
-			<head>
-				<meta charset="utf-8">
-				<title>Introdução ao Node JS</title>
-			</head>
-			<body>
-				<h1>Página de Contato</h1>
-			</body>
-		</html>
-	`);
+	res.render('site/contato');
 });
 
 app.listen(8000, function() {
