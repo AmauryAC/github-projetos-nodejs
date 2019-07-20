1d73c46ab0396098d9e598ee958ccb8bda8905d1
Amaury AC <amaury.costa@hotmail.com>
2019-07-20 18:40:06 -0300

    Implementando a classe NoticiasDAO no modelo utilizando o recurso Prototype no Projeto Portal de Noticias

--- Curso Completo Node JS e MongoDB/curso_node/portal_noticias/config/dbConnection.js
+++ Curso Completo Node JS e MongoDB/curso_node/portal_noticias/config/dbConnection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connMySQL = function() {
-  console.log("Conexão com BD foi estabelecida");
+  //console.log("Conexão com BD foi estabelecida");
 
   return mysql.createConnection({
     host: 'localhost',
