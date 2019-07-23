ef3524c6e3cb85398f50c53bdb09afe219bb2d18
Amaury AC <amaury.costa@hotmail.com>
2019-07-23 11:53:20 -0300

    Adicionando model UsuariosDAO e cadastrando usuarios no BD no Projeto MMORPG

--- Curso Completo Node JS e MongoDB/curso_node/mmorpg_got/app/controllers/cadastro.js
+++ Curso Completo Node JS e MongoDB/curso_node/mmorpg_got/app/controllers/cadastro.js
@@ -17,5 +17,10 @@ module.exports.cadastrar = function(app, req, res) {
     return;
   }
 
+  var connection = app.config.dbConnection;
+  var UsuariosDAO = new app.app.models.UsuariosDAO(connection);
+
+  UsuariosDAO.inserirUsuario(dadosForm);
+
   res.send('Podemos cadastrar');
 };
