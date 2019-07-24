2ec7ffa124799d18bd3e60a2c22838c3483e7e55
Amaury AC <amaury.costa@hotmail.com>
2019-07-24 17:52:46 -0300

    Autenticando usuários com express-session pt2 no Projeto MMORPG

--- Curso Completo Node JS e MongoDB/curso_node/mmorpg_got/app/controllers/jogo.js
+++ Curso Completo Node JS e MongoDB/curso_node/mmorpg_got/app/controllers/jogo.js
@@ -1,3 +1,8 @@
 module.exports.jogo = function(app, req, res) {
-  res.render('jogo');
+  if(req.session.autorizado) {
+    res.render('jogo');
+  }
+  else {
+    res.render('index', {validacao: {}});
+  }
 };
