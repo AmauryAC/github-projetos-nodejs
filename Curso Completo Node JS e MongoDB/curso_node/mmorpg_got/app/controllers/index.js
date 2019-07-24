87f923f916cb1915bc2f71e25b322202227dd57c
Amaury AC <amaury.costa@hotmail.com>
2019-07-24 16:58:24 -0300

    Criando o controller de autenticação no Projeto MMORPG

--- Curso Completo Node JS e MongoDB/curso_node/mmorpg_got/app/controllers/index.js
+++ Curso Completo Node JS e MongoDB/curso_node/mmorpg_got/app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.index = function(app, req, res) {
-  res.render('index');
+  res.render('index', {validacao: {}});
+};
+
+module.exports.autenticar = function(app, req, res) {
+  var dadosForm = req.body;
+
+  req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+  req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+
+  var erros = req.validationErrors();
+
+  if(erros) {
+    res.render('index', {validacao: erros});
+    return;
+  }
+
+  res.send('Tudo OK');
 };
