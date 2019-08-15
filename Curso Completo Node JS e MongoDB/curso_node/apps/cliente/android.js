68d7eb02ca106fb1c0bf15679c11fec4a9b72621
Amaury AC <amaury.costa@hotmail.com>
2019-08-15 09:50:26 -0300

    Iniciando com accept - Content Negociation

--- Curso Completo Node JS e MongoDB/curso_node/apps/cliente/android.js
+++ Curso Completo Node JS e MongoDB/curso_node/apps/cliente/android.js
@@ -1,11 +1,14 @@
 var http = require('http');
 
+var buffer_corpo_response = [];
+
 http.get('http://localhost', function(res) {
   res.on('data', function(pedaco) {
-    console.log(' ' + pedaco);
+    buffer_corpo_response.push(pedaco);
   });
 
   res.on('end', function() {
-    //
+    var corpo_response = Buffer.concat(buffer_corpo_response).toString();
+    console.log(corpo_response);
   });
 });
