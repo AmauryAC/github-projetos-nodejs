2b6f72c2c30f7aac7d077d1df2bd48452921fafb
Amaury AC <amaury.costa@hotmail.com>
2019-08-19 18:28:25 -0300

    Iniciando projeto API RESTful - Instagram clone

---
+++ Curso Completo Node JS e MongoDB/curso_node/instagram_clone/api/server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongodb = require('mongodb');
+
+var app = express();
+
+// Body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = 8080;
+
+app.listen(port);
+
+console.log('Servidor HTTP está escutando na porta ' + port);
+
+app.get('/', function(req, res) {
+  res.send({msg: 'Olá'});
+});
